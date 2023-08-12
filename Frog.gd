a584ae466aba74d4e94ef0b8032d329af1d0c8eb
thing22 <thing22.lamblet@gmail.com>
2023-08-11 18:48:36 -0700

    sync

--- Frog.gd
+++ Frog.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 
 
-
+var s_count=0
 func _on_player_detection_body_entered(body):
-	print(body.name)
-	if body.name == "Player":
-		print("Player")
+	print(body.name, s_count)
+	s_count += 1
+	#if body.name == "Player":
+	if body.name == "CharacterBody2D":
+		print(body.name)
