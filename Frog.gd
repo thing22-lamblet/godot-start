7b3fb54697d116e5796297096f6420b4783df585
thing22 <thing22.lamblet@gmail.com>
2023-08-11 18:33:25 -0700

    sync

--- Frog.gd
+++ Frog.gd
@@ -1,3 +1,9 @@
 extends CharacterBody2D
 
 
+
+
+func _on_player_detection_body_entered(body):
+	print(body.name)
+	if body.name == "Player":
+		print("Player")
--- Player/Player.gd
+++ Player/Player.gd
@@ -21,7 +21,6 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	#As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	print(direction)
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
 	elif direction == 1: 
