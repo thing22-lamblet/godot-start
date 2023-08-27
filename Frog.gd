b6b73860baa6900dd0c333987afc0cfe6a082dce
thing22 <thing22.lamblet@gmail.com>
2023-08-27 13:49:44 -0700

    sync

--- Frog.gd
+++ Frog.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player
 
+func _physics_process(delta):
+	velocity.y += gravity * delta
 
-var s_count=0
+	move_and_slide()
+	
 func _on_player_detection_body_entered(body):
-	print(body.name, s_count)
-	s_count += 1
-	#if body.name == "Player":
-	if body.name == "CharacterBody2D":
-		print(body.name)
+	if body.name == "Player":
+		print("Player")
+		player = get_node("../../Player/Player")
+		var direction = (player.postion - self.position).normalized()
+		if direction.x > 0:
+			
+			print("Right")
+		else:
+			print("Left")
