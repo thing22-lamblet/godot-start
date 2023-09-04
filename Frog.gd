08ab5275fc2ba815cbd19ef7399609151cc847cf
thing22 <thing22.lamblet@gmail.com>
2023-09-04 14:04:21 -0700

    sync

--- Frog.gd
+++ Frog.gd
@@ -1,24 +1,39 @@
 extends CharacterBody2D
 
+var SPEED = 50
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
-
+var chase = false
 func _physics_process(delta):
+	#Gravity for frog
 	velocity.y += gravity * delta
-
-	move_and_slide()
+	if chase == true:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Jump")
+			player = get_node("../../Player/Player")
+			var direction = (player.position - self.position).normalized()
+			if direction.x > 0:
+				get_node("AnimatedSprite2D").flip_h = true
+			else:
+				get_node("AnimatedSprite2D").flip_h = false
+			velocity.x = direction.x * SPEED
+		else:
+			if get_node("AnimatedSprite2D").animation != "Death":
+				get_node("AnimatedSprite2D").play("Resting")
+			velocity.x = 0
+		move_and_slide()
 
 func _on_player_detection_body_entered(body):
 	if body.name == "Player":
-		print("Player")
-		player = get_node("../../Player/Player")
-		print(player.position)
+		chase = true
 
-		#look_at(player.position)
-		var direction = (player.position - self.position).normalized()
-		print(direction)
-		if direction.x > 0:
-			print("Right")
-		else:
-			print("Left")
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
 
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		get_node("AnimatedSprite2D").play("Death")
+		await get_node("AnimatedSprite2D").animation_finished
+		self.queue_free()
